deedd2461f6fbbecc3890c44c9c3fb8b989a6a90
javidtvab <165400423+javidtvab@users.noreply.github.com>
2024-12-21 17:18:46 +0100

    Create __init__.py

---
+++ backend/app/utils/__init__.py
@@ -0,0 +1,5 @@
+# app/utils/__init__.py
+
+# Este archivo agrupa todas las utilidades disponibles en `utils`.
+from .razorpay_config import razorpay_client
+from .pdf_generator import generate_invoice_pdf
