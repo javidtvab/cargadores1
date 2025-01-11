ac4841081e5799d6465b31e7d3f7119c3077911a
javidtvab <165400423+javidtvab@users.noreply.github.com>
2025-01-11 12:25:58 +0100

    Delete backend directory

--- backend/app/__init__.py
+++
@@ -1,3 +0,0 @@
-# app/__init__.py
-
-# Este archivo hace que la carpeta `app` sea tratada como un paquete de Python.
--- backend/app/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# app/utils/__init__.py
-
-# Este archivo agrupa todas las utilidades disponibles en `utils`.
-from .razorpay_config import razorpay_client
-from .pdf_generator import generate_invoice_pdf
